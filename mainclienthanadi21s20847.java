67e9259068bb711f0961026082b62b8c4fb8c085
HANADIJUMA <134373517+HANADIJUMA@users.noreply.github.com>
2023-05-31 10:21:04 -0700

    Create mainclienthanadi21s20847.java

---
+++ mainclienthanadi21s20847.java
@@ -0,0 +1,10 @@
+public class mainClienthanadijuma21s20847
+{
+public static void main(String args[])
+{
+    clienthanadijuma21s20847 Hanadicr1 =new clienthanadijuma21s20847();
+Thread Hanadist = new Thread(Hanadicr1);
+
+Hanadist.start();
+}
+}

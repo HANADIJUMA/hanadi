2be668b3eb3f8e172a078f45fe53041cc8440c4c
HANADIJUMA <134373517+HANADIJUMA@users.noreply.github.com>
2023-05-31 10:23:58 -0700

    Create mainServerhanadijuma21s20847.java

---
+++ mainServerhanadijuma21s20847.java
@@ -0,0 +1,10 @@
+public class mainServerhanadijuma21s20847
+{
+public static void main(String args[], Thread se)
+{
+Serverhanadi21s20847 hanadiss=new Serverhanadi21s20847();
+
+Thread Hanadisn=se Thread((Runnable) hanadisn);
+Hanadisn.start();
+}
+}
